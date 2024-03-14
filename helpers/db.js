32b701aeacf4a4623b98d4f2f43ab7d7139d68fd
Daria Zakharova <t3zada00@students.oamk.fi>
2024-03-14 14:19:18 +0200

    Fix typo in host variable

--- helpers/db.js
+++ helpers/db.js
@@ -16,7 +16,7 @@ const query = async (sql, values = []) => {
 const openDb = ()=> {
     const pool = new Pool({
         user: process.env.DB_USER,
-        host: process.env.HOST,
+        host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
